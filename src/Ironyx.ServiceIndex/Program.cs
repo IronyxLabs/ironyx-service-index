ad1d8ff1f660efaeb45497aaba9808861c72dbc0
Bence Mihucza <mihben@gmail.com>
2026-07-25 16:18:12 +0200

    chore: Integrate Serilog

--- src/Ironyx.ServiceIndex/Program.cs
+++ src/Ironyx.ServiceIndex/Program.cs
@@ -1,5 +1,9 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSerilog((_, configuration) =>
+configuration.ReadFrom.Configuration(builder.Configuration));
 
 var app = builder.Build();
 
