4bf3aa230f19d0a17413e00fdbc1aa1e94d80a0c
Bence Mihucza <mihben@gmail.com>
2026-07-26 00:59:41 +0200

    chore: Integrate Kernel

--- src/Ironyx.ServiceIndex/Program.cs
+++ src/Ironyx.ServiceIndex/Program.cs
@@ -1,11 +1,15 @@
+
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSerilog((_, configuration) =>
-configuration.ReadFrom.Configuration(builder.Configuration));
+builder.Services.AddSerilog((_, configuration) => configuration.ReadFrom.Configuration(builder.Configuration));
+
+builder.UseKernel()
+    .AddCommandSender(new Uri("http://localhost:5100/"));
 
 var app = builder.Build();
 
+app.MapKernel();
 
 app.Run();
